238aeacbdde8dcdc6845ffa8bd6c861db8359dc5
Aaron Yarborough <me@aaronjy.me>
2020-05-01 22:24:46 +0100

    Construct view models on init()

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,28 @@ import { Calendar } from "./models/calendar.model";
 import { Event } from "./models/event.model";
 
 import calendars from "./calendars";
+import { EventViewModel } from "./models/event.viewmodel";
 
 class App {
     static async init() {
         const today: Date = this.getTodayMidnight();
         const tomorrow: Date = this.getTomorrowMidnight();
 
+        // Think of something cleaner than just calling flat()
         const events = (await CalenderService.getCalendars(calendars))
-            .map((calendar: Calendar) => {
-                return calendar.events.filter((event: Event) => {
-                    return event.occursBetween(today, tomorrow);
-                });
-            });
+            .map((calendar: Calendar) => calendar.events
+                .filter((event: Event) => event.occursBetween(today, tomorrow))
+                .map((event: Event) => {
+                    return {
+                        calendarName: calendar.name,
+                        startDate: event.startDate,
+                        endDate: event.endDate,
+                        summary: event.summary
+                    } as EventViewModel
+                })
+            ).flat();
         
-        console.log([].concat.apply(events[0]));
+        console.log(events);
     }
 
     private static getTodayMidnight(): Date {
